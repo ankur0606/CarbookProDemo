bf710768f83f9f393d2ef3c6fc4d7785f392e87e
Ankur Jain <ankur.jain@autodatacorp.org>
2016-08-16 11:39:43 -0700

    made cbp brand generic

--- src/test/java/com/chromedata/carbookPro/pages/LoginPage.java
+++ src/test/java/com/chromedata/carbookPro/pages/LoginPage.java
@@ -2,10 +2,10 @@ package com.chromedata.carbookPro.pages;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
-import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.annotations.At;
 import net.thucydides.core.pages.PageObject;
 
-@DefaultUrl("login.jsp")
+
 public class LoginPage extends PageObject {
 
 	@FindBy(name = "username")
